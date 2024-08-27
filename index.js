ceded2cb22822fb5941c983984e3ad0f053fc86f
8ad40n <joy184110@gmail.com>
2024-08-28 01:18:45 +0600

    initial setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY);
+const model = genAI.getGenerativeModel({ model: process.env.AI_MODEL});
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 6000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +15,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+async function run() {
+    const prompt = ""
+  
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+  
+run();
