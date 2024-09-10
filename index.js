8b66a74f0eaae056a4ad11124d6edd28b1795c26
8ad40n <joy184110@gmail.com>
2024-09-11 00:50:38 +0600

    cors

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require('express');
 const connectDB = require("./src/config/db");
 const userRoutes = require("./src/routes/user.route");
 const generateRoutes = require("./src/routes/generate.route");
 const app = express();
-const port = 6000;
+const port = 5000;
 
 app.use(express.json());
+
+// app.use(cors({
+//   origin: ["http://localhost:3000"],
+//   credentials: true
+// }));
+
+app.use(cors());
 
 // connect to MongoDB:
 connectDB();
